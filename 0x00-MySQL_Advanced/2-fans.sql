37056ec6965c9cf512fae6ab4ae8f9bfc9cc8007
Ano-ly <amarachiuvere@student.oauife.edu.ng>
2024-06-14 05:55:52 +0100

    Task 2 edit

--- 0x00-MySQL_Advanced/2-fans.sql
+++ 0x00-MySQL_Advanced/2-fans.sql
@@ -1,4 +1,4 @@
 -- Solve a sorting problem
 -- Select statement to select and group results
 
-SELECT origin, SUM(fans) AS nb_fans FROM metal_bands GROUP BY origin ORDER BY NULL;
+SELECT origin, SUM(fans) AS nb_fans FROM metal_bands GROUP BY origin ORDER BY nb_fans DESC;
