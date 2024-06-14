f615b46e075daa2eaa3ce86cfe959738bd5cddb4
Ano-ly <amarachiuvere@student.oauife.edu.ng>
2024-06-14 05:24:29 +0100

    Set up

---
+++ 0x00-MySQL_Advanced/0-uniq_users.sql
@@ -0,0 +1,8 @@
+-- Create user table with id, email and name
+-- Create the table
+
+CREATE TABLE IF NOT EXISTS users (
+	id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+	email CHAR(255) NOT NULL UNIQUE,
+	name CHAR(255)
+)
