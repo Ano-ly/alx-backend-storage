92235eff951a6e1aef0f63accc49f3768ed0d231
Ano-ly <amarachiuvere@student.oauife.edu.ng>
2024-06-20 05:12:05 +0100

    Edit task 9 file

--- 0x01-NoSQL/9-insert_school.py
+++ 0x01-NoSQL/9-insert_school.py
@@ -5,8 +5,5 @@
 def insert_school(mongo_collection, **kwargs):
     """Create a document with kwargs and add to collection"""
 
-    document1 = dict()
-    for item, value in kwargs.items():
-        document1.update({item: value})
-    my_id = mongo_collection.insert_one(document1)
-    return (my_id)
+    my_id = mongo_collection.insert_one(kwargs)
+    return (my_id.inserted_id)
