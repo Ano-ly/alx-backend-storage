9b1bcc31f8874e385220b5f51173f8c4d921c64f
Ano-ly <amarachiuvere@student.oauife.edu.ng>
2024-06-20 02:42:58 +0100

    Complete task 10

---
+++ 0x01-NoSQL/10-update_topics.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+"""Function that creates a dcocument with key-value pairs"""
+
+
+def update_topics(mongo_collection, name, topics):
+    """Add/ update topics in document"""
+
+    mongo_collection.update_one({"name": name}, {"$inc": {"topics": topics}})
