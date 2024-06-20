9c5a78f194e36b1232131e06ed7ce9c23df6ea4c
Ano-ly <amarachiuvere@student.oauife.edu.ng>
2024-06-20 02:09:11 +0100

    Complete task 8

---
+++ 0x01-NoSQL/8-all.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+"""Use pymongo to work with MongoDB database"""
+
+
+def list_all(mongo_collection):
+    """List all documents in a collection"""
+    ret_list = []
+    for doc in mongo_collection.find():
+        ret_list.append(doc)
+    return (doc)
