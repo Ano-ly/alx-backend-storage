4bc3cad7426ffa0a6b01c5e052d1b2c920f54c06
Ano-ly <anomalie23.0@gmail.com>
2024-06-19 18:31:12 -0700

    Complete task 9

--- 0x01-NoSQL/9-insert_school.py
+++ 0x01-NoSQL/9-insert_school.py
@@ -1,11 +1,12 @@
 #!/usr/bin/env python3
 """Function that creates a dcocument with key-value pairs"""
 
+
 def insert_school(mongo_collection, **kwargs):
     """Create a document with kwargs and add to collection"""
 
     document1 = dict()
-    for item, value in kwargs:
-       document1.update({item: value})
-    my_id = mongo_collection.insertOne(document1)
+    for item, value in kwargs.items():
+        document1.update({item: value})
+    my_id = mongo_collection.insert_one(document1)
     return (my_id)
